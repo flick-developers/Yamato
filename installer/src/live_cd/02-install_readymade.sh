d5f60e1e54d1b3d738ac6f9a3333a51d27b3f849
Dominic Parry <161527865+why-context@users.noreply.github.com>
2026-09-17 00:14:42 +0100

    FIX: wrong package name. 404.

--- installer/src/live_cd/02-install_readymade.sh
+++ installer/src/live_cd/02-install_readymade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dnf install -y --setopt=install_weak_deps=False --allowerasing readymade-nightly \
+dnf install -y --setopt=install_weak_deps=False --allowerasing readymade-git \
   libblockdev-btrfs libblockdev-lvm libblockdev-dm langpacks-core-en langpacks-fonts-en im-chooser
 
 rm -f /usr/share/applications/liveinst.desktop
--- installer/src/live_cd/98-prep_titanoboa.sh
+++ installer/src/live_cd/98-prep_titanoboa.sh
@@ -2,7 +2,12 @@
 
 mkdir -p /boot/efi
 mkdir -p /usr/lib/bootc-image-builder
+mkdir -p /etc/bootcwholedisk
+mkdir -p /etc/wholedisk
+
 cp -av /usr/lib/efi/*/*/EFI /boot/efi/ || true
 cp -v /boot/efi/EFI/fedora/grubx64.efi /boot/efi/EFI/BOOT/fbx64.efi || true
 cp /src/iso.yaml /usr/lib/bootc-image-builder/iso.yaml
 cp /src/readymade.toml /etc/readymade.toml
+cp -r /src/wholedisk /etc/bootcwholedisk/
+cp -r /src/wholedisk /etc/wholedisk/
