6364283460a3453c51f398478721b9a3896e2348
Dominic Parry <161527865+why-context@users.noreply.github.com>
2026-09-16 23:27:41 +0100

    FIX: DNF was not setup right in one file.

--- installer/src/build.sh
+++ installer/src/build.sh
@@ -31,7 +31,7 @@ systemctl enable livesys.service livesys-late.service
 dnf install -y dnf5-plugins
 
 for script in /src/live_cd/*.sh; do
-  echo -e "\033[1;34m::\033[0m $script"
+  echo -e "\e[1;32m --> \e[0m $script"
   bash "$script"
 done
 
