fbfe1fe5f85f757f1100c9e4ac7393d256f9d04d
Dominic Parry <why_context@pm.me>
2026-09-17 18:52:51 +0100

    RESET: Prep for resetting branch

--- installer/src/live_cd/02-install_readymade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dnf install -y --setopt=install_weak_deps=False --allowerasing readymade-git \
-  libblockdev-btrfs libblockdev-lvm libblockdev-dm langpacks-core-en langpacks-fonts-en im-chooser
-
-rm -f /usr/share/applications/liveinst.desktop
-sed -i '/NoDisplay=.*/d' /usr/share/applications/com.fyralabs.Readymade.desktop
-cp -f /usr/share/applications/com.fyralabs.Readymade.desktop /etc/xdg/autostart
